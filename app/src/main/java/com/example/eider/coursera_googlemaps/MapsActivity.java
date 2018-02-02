fb9ea5b215265c782838300971c4dc8d64b7be40
eider <eiderdiazmoraila@hotmail.com>
2018-02-02 15:52:23 -0700

    arreglado marcador custom

--- app/src/main/java/com/example/eider/coursera_googlemaps/MapsActivity.java
+++ app/src/main/java/com/example/eider/coursera_googlemaps/MapsActivity.java
@@ -1,12 +1,24 @@
 package com.example.eider.coursera_googlemaps;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -14,6 +26,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     String lugar;
+    int resourseDeImagen;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,28 +40,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (getIntent().getExtras().getString("playa") != null){
          lugar  = getIntent().getExtras().getString("playa");
+            resourseDeImagen=  R.drawable.beach;
         }
         if (getIntent().getExtras().getString("cerro") != null){
             lugar  = getIntent().getExtras().getString("cerro");
+            resourseDeImagen=  R.drawable.mountains;
+
         }
         if (getIntent().getExtras().getString("road") != null){
             lugar  = getIntent().getExtras().getString("road");
+            resourseDeImagen=  R.drawable.road;
+
         }
         if (getIntent().getExtras().getString("parque") != null){
             lugar  = getIntent().getExtras().getString("parque");
+            resourseDeImagen=  R.drawable.valley;
+
         }
     }
 
+    private Bitmap getMarkerBitmapFromView(@DrawableRes int resId) {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near place, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    View customMarkerView = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.view_custom_marker, null);
+    ImageView markerImageView = (ImageView) customMarkerView.findViewById(R.id.profile_image);
+        markerImageView.setImageResource(resId);
+        customMarkerView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        customMarkerView.layout(0, 0, customMarkerView.getMeasuredWidth(), customMarkerView.getMeasuredHeight());
+        customMarkerView.buildDrawingCache();
+    Bitmap returnedBitmap = Bitmap.createBitmap(customMarkerView.getMeasuredWidth(), customMarkerView.getMeasuredHeight(),
+            Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(returnedBitmap);
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+    Drawable drawable = customMarkerView.getBackground();
+        if (drawable != null)
+            drawable.draw(canvas);
+        customMarkerView.draw(canvas);
+        return returnedBitmap;
+}
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -69,6 +99,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-        mMap.addMarker(new MarkerOptions().position(place).title("Marker in place"));
+        mMap.addMarker(new MarkerOptions()
+                .position(place).title("Marker in place")
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(resourseDeImagen))));
     }
 }
